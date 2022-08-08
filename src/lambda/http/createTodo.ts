8d4db4a979dd454f1d53769222616e36a47359e6
debola_is <adebolasanni079@gmail.com>
2022-08-08 15:35:25 +0100

    create todo implemented, not tested yet

--- src/lambda/http/createTodo.ts
+++ src/lambda/http/createTodo.ts
@@ -3,19 +3,37 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import {TodoItem} from '../../models/TodoItem'
 import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { createTodoForUser } from '../../helpers/todos'
 
-export const handler = middy(
+export const handler = 
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log ("Processing Event: ", event)
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    
     // TODO: Implement creating a new TODO item
+    const newItem = await createTodoForUser(newTodo, event)
+   try {
+    return {
+      statusCode: 201,
+      headers: {
+          "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+          newItem
+      })
+    }
+   }
+   catch(e) {
+    return {
+      statusCode: 400,
+      headers: {
+          "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify("Could not create this todo")
+    }
+  }
 
-    return undefined
-)
-
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+    
+}
