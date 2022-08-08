a075cf4d79b32764890a3dfb8418e69314b5065b
debola_is <adebolasanni079@gmail.com>
2022-08-08 15:29:22 +0100

    create todo implemented, not tested

--- src/helpers/todos.ts
+++ src/helpers/todos.ts
@@ -1,11 +1,11 @@
 import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
 import * as createError from 'http-errors'
+import { getUserId } from '../lambda/utils';
 
 // TODO: Implement businessLogic
 
@@ -14,3 +14,43 @@ const todosAccess = new TodosAccess()
 export async function getTodosForUser(userId): Promise<TodoItem[]> {
     return todosAccess.getUserTodos(userId)
 }
+export async function createTodoForUser(createTodoRequest: CreateTodoRequest, event): Promise<TodoItem> {
+
+    //Generate new uuid for new tdo Item
+    const itemId = uuid.v4()
+    const userId = getUserId(event)
+
+    const createdTodo = await todosAccess.createTodo({
+        todoId: itemId,
+        userId: userId,
+        createdAt: new Date().toISOString(),
+        name: createTodoRequest.name,
+        dueDate: createTodoRequest.dueDate,
+        done: false,
+        attachmentUrl: "Not implemented"
+    })
+
+    return todosAccess.createTodo(createdTodo)
+}
+
+/**
+ * 
+ * createdAt": "2019-07-27T20:01:45.424Z",
+ "name": "Buy milk",
+ "dueDate": "2019-07-29T20:01:45.424Z",
+ "done": false,
+ "attachmentUrl": "http://example.com/image.png"
+}
+
+
+{
+ "item": {
+  "todoId": "123",
+  "createdAt": "2019-07-27T20:01:45.424Z",
+  "name": "Buy milk",
+  "dueDate": "2019-07-29T20:01:45.424Z",
+  "done": false,
+  "attachmentUrl": "http://example.com/image.png"
+}
+}
+ */
